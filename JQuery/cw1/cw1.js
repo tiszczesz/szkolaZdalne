85ebdb294eec673fd7c7982e8fc5f22dd6e5f50b
pszczesz <pszczesz@gmail.com>
2020-03-29 22:15:51 +0200

    d1

---
+++ JQuery/cw1/cw1.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#wynik").click(function(){
+        //zmiana tekstu po kliknięciu akapitu
+        let text = $(this).html(); //odpowiednik innerHTML
+        let wynik = "Hello from jQuery!!!";
+        if(text!=wynik){
+            $(this).html(wynik).css("background-color", "blue");
+
+        }else{
+            $(this).html("Tutaj się zmieni po kliknięiu na tekście")
+            .css("background-color","brown");
+        }
+        //możliwośc używania css po kropce w wywołaniu łancuchowym poniważ
+        //każde użycie $() zwraca obiekt na, którym pracujemy (akapit)
+        //nie należy mieszać instrukcji czystego JS i jQuery!!! (np innerHTML - html())
+    });
+});
