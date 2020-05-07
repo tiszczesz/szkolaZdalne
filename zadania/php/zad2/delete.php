c47323ec3c86db18c9f9d5614b56b5977d52416f
pszczesz <pszczesz@gmail.com>
2020-05-07 12:39:12 +0200

    d2

---
+++ zadania/php/zad2/delete.php
@@ -0,0 +1,14 @@
+<?php
+require_once "functions.php";
+if(filter_has_var(INPUT_GET,'id')){
+    //echo "Usuwanie rekordu o id ={$_GET['id']} <br>";
+    $id = filter_input(INPUT_GET,'id',FILTER_VALIDATE_INT);//$id = $_GET['id'];
+    // if(deleteRow($id)){
+    //     echo "Usunięto rekord o id: {$id}<br>";
+     //       echo "<div><a href='cw1.php'>Powrót</a></div>"
+    // }else{
+    //     echo "Nie można usunąć rekordu o id: {$id}<br>";
+    // }
+        deleteRow($id);
+}
+header("Location: cw1.php");
