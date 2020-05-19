3cbd6a4710764a445bb499de8972dacb9dadba05
pszczesz <pszczesz@gmail.com>
2020-05-19 10:39:12 +0200

    d1

--- Mysql_Cwiczenia/1pg_cwiczenia/cw4.sql
+++ Mysql_Cwiczenia/1pg_cwiczenia/cw4.sql
@@ -6,7 +6,7 @@ create table kategorie(
     opis varchar(150)
 );
 show tables;
-
+use cw2_1pg;
 describe kategorie;
 
 insert into kategorie(nazwa,opis)
@@ -35,3 +35,50 @@ where kategoriaId is null;
 
 alter table kategorie
 modify column opis varchar(50);
+
+
+-------------------- klucze obce
+describe towary;
+select * from towary;
+select * from kategorie;
+insert into towary(nazwa,cena,kategoriaId)
+values("testowy",9.99,5);
+
+Alter table towary
+add FOREIGN key (kategoriaId) references kategorie(id);
+delete from towary where id=10;
+describe towary;
+
+create table rodzaje(
+    id int primary key auto_increment,
+    nazwa varchar(50) not null
+)
+create table filmy(
+    id int primary key auto_increment,
+    tytul varchar(50) not null,
+    cena decimal(10,2),
+    rodzajId int,
+    CONSTRAINT FK_rodzaj_film FOREIGN KEY (rodzajId)
+    REFERENCES rodzaje(id)
+)
+insert into filmy(tytul,cena,rodzajId)
+values("kwakwa",34.99,null);
+select * from filmy;
+drop table filmy;
+
+create table filmy(
+    id int primary key auto_increment,
+    tytul varchar(50) not null,
+    cena decimal(10,2),
+    rodzajId int not null,
+    CONSTRAINT FK_rodzaj_film FOREIGN KEY (rodzajId)
+    REFERENCES rodzaje(id)
+)
+insert into rodzaje(nazwa) values ("wojenny");
+insert into rodzaje(nazwa) values ("komediowy");
+insert into rodzaje(nazwa) values ("obyczajowy");
+insert into rodzaje(nazwa) values ("kryminalny");
+select * from rodzaje;
+insert into filmy(tytul,cena,rodzajId)
+values("kwakwa",34.99,1);
+select * from filmy;
